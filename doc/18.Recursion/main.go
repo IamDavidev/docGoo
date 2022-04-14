73a38fd2dc3428e8d8420552146e4a0000a60232
itsDavidev <davidlez2mgk@gmail.com>
2022-04-14 14:54:04 -0400

    - expon sin bucles

--- doc/18.Recursion/main.go
+++ doc/18.Recursion/main.go
@@ -1,1 +1,27 @@
-package recursion
+package main
+
+import "fmt"
+
+var Esponte int = 0
+
+func main() {
+	Expon(3, 3, 0)
+
+}
+
+// 2 2 = 4
+// 2 3 = 8
+func Expon(num int, exp int, i int) {
+	if i == 0 {
+		Esponte = num
+	}
+	if i == exp-1 {
+		return
+	}
+
+	var resultado = num * Esponte
+
+	fmt.Println(num, " * ", Esponte, "=", resultado)
+
+	Expon(resultado, exp, i+1)
+}
